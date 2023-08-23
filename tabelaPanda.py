95b17755a3b847639969d1e2cc8b2bac6128a992
mauriciomenon <54405514+mauriciomenon@users.noreply.github.com>
2023-08-22 22:23:41 -0300

    fonte maior

--- tabelaPanda.py
+++ tabelaPanda.py
@@ -14,18 +14,37 @@ data = [
 ]
 
 table_str = tabulate(data, headers="firstrow", tablefmt="grid")
-font = ImageFont.load_default()
+font_size = 20
+font_path = "/Library/Fonts/Arial.ttf"  # Change this to the correct path on your system
+font = ImageFont.truetype(font_path, font_size)
 
-# Calculating the maximum width and height of cells
-cell_width = max(font.getbbox(cell)[2] for row in data for cell in row)
-cell_height = font.getbbox(table_str.splitlines()[0])[3]
+cell_widths = [max(font.getbbox(cell)[2] for cell in column) for column in zip(*data)]
+cell_height = font.getbbox(table_str.splitlines()[0])[3] + 10  # Adding padding
 
-table_width = cell_width * len(data[0])
+table_width = sum(cell_widths)
 table_height = cell_height * len(data)
 
 image = Image.new("RGBA", (table_width, table_height), (255, 255, 255, 255))
 draw = ImageDraw.Draw(image)
-draw.text((0, 0), table_str, fill="black", font=font)
+
+y_position = 0
+for row in data:
+    x_position = 0
+    for cell, width in zip(row, cell_widths):
+        draw.text((x_position, y_position), cell, fill="black", font=font)
+        x_position += width
+    y_position += cell_height
+
+# Desenhar linhas horizontais
+for i in range(len(data) + 1):
+    y = i * cell_height
+    draw.line((0, y, table_width, y), fill="black", width=1)
+
+# Desenhar linhas verticais
+x_position = 0
+for width in cell_widths:
+    draw.line((x_position, 0, x_position, table_height), fill="black", width=1)
+    x_position += width
 
 image.save("styled_table.png")
 image.show()
